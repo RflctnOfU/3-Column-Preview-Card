16220696a44241ff4e15316c681979b5801b938f
Kris Marshall <rflctnofu@hotmail.com>
2023-07-08 11:59:43 -0400

    configured fonts, finished the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Lexend: ["Lexend Deca"],
+        Big: ["Big Shoulders Display"],
+      },
+    },
   },
   plugins: [],
 };
